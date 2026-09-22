3485b8799a194c6a6767d3f9ba1e3e169c0b8f72
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-22 13:46:52 -0300

    Fix payphone mission interaction

---
+++ scripts/world/payphone.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+@export var active_action_text := "Atender telefone"
+@export var idle_action_text := "Usar telefone"
+
+
+func _ready() -> void:
+	add_to_group("interactable")
+
+
+func get_interaction_text(_player: CharacterBody2D) -> String:
+	if MissionManager.stage == MissionManager.Stage.ANSWER_PHONE:
+		return active_action_text
+	return idle_action_text
+
+
+func interact(player: CharacterBody2D) -> void:
+	var text := MissionManager.answer_phone()
+	player.call("show_message", text)
